e65d20b94b181a7bea2b0f25fc595dbdd374d7e0
David William Bull <116688139+Zenefess@users.noreply.github.com>
2022-12-05 14:53:45 +1100

    Update gs.instanced.sprite_arrays.hlsl
    Added: *Normal mapping. *Child sprites automatically transrotate & scale relative to parent sprite's orientation & scale. *Maximum sprite array size increased to 32.
    
    Fixed: *Occlusion map not overriding highlight map. *Sprites drawn in forward order maximises overdraw.

--- gs.instanced.sprite_arrays.hlsl
+++ gs.instanced.sprite_arrays.hlsl
@@ -1,6 +1,6 @@
 /************************************************************
  * File: gs.instanced.sprites.hlsl      Created: 2022/11/21 *
- *                                    Last mod.: 2022/11/28 *
+ *                                    Last mod.: 2022/12/04 *
  *                                                          *
  * Desc: Geometry shader. Generates 3-space sprites from    *
  *       point lists. Animation support, optional billboard *
@@ -29,7 +29,7 @@ struct PART_IMM {	// 68 bytes
 	float3	t_rot;	// Slide & rotation vectors (maximum transformation)
 	uint		bits;		// 0==Shape (quad/tri), 1==Billboard
 	uint		tc[2];	// tc[1].r == Paint map | ts[1].g == Emission map (+0.5f)
-};							// tc[1].b == Phong map | ts[1].a == Damage map
+};							// tc[1].b == Highlight map | ts[1].a == Occlusion map
 
 struct BONE_DYN {	// 56 bytes
 	float3	pos;
@@ -48,66 +48,90 @@ StructuredBuffer<BONE_DYN> bone;
 struct GOut {	// 13 scalars
 	float4 pos : SV_Position;
 	float3 position : POSITION;
-	float3 n : NORMAL;
 	float2 tex : TEXCOORD;
-	uint   i : BLENDINDICES;		// Sprite index
+	uint   i : BLENDINDICES0;		// Sprite index
+	uint   rotX : BLENDINCICES1;	// sin & cos of X rotation
+	uint   rotY : BLENDINCICES2;	// sin & cos of Y rotation
+	uint   rotZ : BLENDINCICES3;	// sin & cos of Z rotation
 };
 
 inline float2 RotateVector(float2 v, float2 dir) { return float2(dot(v, float2(dir.x, -dir.y)), dot(v, dir.yx)); }
 
 // index[0]: 0-11==Object index (~4K unique), 12-31==Parent bone index (~1,024K unique)
-[instance(19)] [maxvertexcount(4)]
+[instance(32)] [maxvertexcount(4)]
 void main(point uint index[1] : BLENDINDICES, uint pID : SV_PrimitiveID, uint iID : SV_GSInstanceID, inout TriangleStream<GOut> triStream) {
+	GOut   output;
+	float2 fRot;
 	// Abandon quad if infinitely small
-	if(bone[index[0]].size.x == 0.0f) return;
-	// Unpack array indices
-	const uint   uiOAI      = index[0] & 0x0FFF;
-	const uint   uiBAI      = (index[0] >> 12) + iID;
-	const uint   uiSAI      = (bone[uiBAI].afo_sai & 0x0FFFFF);
-	// Unpack object header bits
-	const uint   uiQuadC    = object[uiOAI] >> 27;
-	const	uint   uiPAI      = (object[uiOAI] & 0x0FFFF) + iID;
+	if(bone[index[0] >> 12].size.x == 0.0f) return;
+	// Unpack object array index from input data
+	const uint uiOAI = index[0] & 0x0FFF;
+	// Unpack quad count from object header bits
+	const uint uiQuadC = object[uiOAI] >> 27;
 	// Abandon quad if part is non-existant
 	if(uiQuadC < iID) return;
+	// Unpack array indices
+	const uint uiBPI = index[0] >> 12;
+	const uint uiBAI = uiBPI + uiQuadC - iID;
+	const uint uiSAI = (bone[uiBAI].afo_sai & 0x0FFFFF);
+	const uint uiPPI = object[uiOAI] & 0x0FFFF;
+	const uint uiPAI = uiPPI + uiQuadC - iID;
 	// Unpack bone bits
-	const float  fAFO       = float((bone[uiBAI].afo_sai >> 24) & 0x0FF);
-	const float  fAFC       = float(((bone[uiBAI].afc_ft >> 24) & 0x0FF) + 1);
-	const float  fFT        = float(bone[uiBAI].afc_ft & 0x0FF) * 0.00390625f + float((bone[uiBAI].afc_ft >> 8) & 0x0FFFF);
+	const float fAFO = float((bone[uiBAI].afo_sai >> 24) & 0x0FF);
+	const float fAFC = float(((bone[uiBAI].afc_ft >> 24) & 0x0FF) + 1);
+	const float fFT  = float(bone[uiBAI].afc_ft & 0x0FF) * 0.00390625f + float((bone[uiBAI].afc_ft >> 8) & 0x0FFFF);
 	// Unpack part bits
 	const bool   bShape     = part[uiPAI].bits & 0x01;
 	const bool   bBillboard = (part[uiPAI].bits >> 1) & 0x01;
-	// Calculate position, rotation, and size
-			float3 fPosOS     = bone[index[0]].pos; if(iID) fPosOS += (part[uiPAI].pos + bone[uiBAI].pos) * bone[index[0]].size;
-			float3 fRotOS     = bone[index[0]].rot; if(iID) fRotOS += bone[uiBAI].rot;
-	const float3 fPosition  = fPosOS + lerp(part[uiPAI].pos, part[uiPAI].t_pos, bone[uiBAI].lerp);
-	const float3 fRotation  = fRotOS + lerp(part[uiPAI].rot, part[uiPAI].t_rot, bone[uiBAI].lerp);
-			float3 fVert1     = bone[uiBAI].size * float3(part[uiPAI].size, 0.0f); if(iID) fVert1 *= bone[index[0]].size;
-			float3 fVert2     = float3(-fVert1.x, fVert1.yz);
+	// Calculate adjusted position and rotation
+	float3 fPosition = lerp(part[uiPAI].pos, part[uiPAI].t_pos, bone[uiBAI].lerp);
+	float3 fRotation = lerp(part[uiPPI].rot + bone[uiBPI].rot, part[uiPPI].t_rot + bone[uiBPI].rot, bone[uiBPI].lerp);
+	// Transrotate if child
+	if(uiQuadC - iID) {
+		fPosition *= float3(part[uiPPI].size, 1.0f) * bone[uiBPI].size * 0.25f;
+		sincos(fRotation.z * 0.5f, fRot.y, fRot.x);
+		fPosition.xy = RotateVector(fPosition.xy, fRot);
+		fPosition.xy = RotateVector(fPosition.xy, fRot);
+		sincos(fRotation.y * 0.5f, fRot.y, fRot.x);
+		fPosition.xz = RotateVector(fPosition.xz, fRot);
+		fPosition.xz = RotateVector(fPosition.xz, fRot);
+		sincos(fRotation.x * 0.5f, fRot.y, fRot.x);
+		fPosition.yz = RotateVector(fPosition.yz, fRot);
+		fPosition.yz = RotateVector(fPosition.yz, fRot);
+
+		fRotation += lerp(part[uiPAI].rot + bone[uiBAI].rot, part[uiPAI].t_rot, bone[uiBAI].lerp);
+	}
+	fPosition += lerp(part[uiPPI].pos + bone[uiBPI].pos, part[uiPPI].t_pos + bone[uiBPI].pos, bone[uiBPI].lerp);
+	// Calculate size
+	float3 fVert1 = bone[uiBAI].size * float3(part[uiPAI].size, 0.0f); if(iID) fVert1 *= bone[uiBPI].size;
+	float3 fVert2 = float3(-fVert1.x, fVert1.yz);
 	// Unpack texture coordinates
-	const float4 fTC        = float4((bone[uiBAI].tc[0] >> uint2(0, 16)) & 0x0FFFF, (bone[uiPAI].tc[1] >> uint2(0, 16)) & 0x0FFFF) * 0.000030517578125f;
+	const float4 fTC = float4((bone[uiBAI].tc[0] >> uint2(0, 16)) & 0x0FFFF, (bone[uiPAI].tc[1] >> uint2(0, 16)) & 0x0FFFF) * 0.000030517578125f;
 
 	// Rotate
-	float2 fRot[3];
-	sincos(fRotation.z, fRot[0].y, fRot[0].x);
-	fVert1.xy = RotateVector(fVert1.xy, fRot[0]);
-	fVert2.xy = RotateVector(fVert2.xy, fRot[0]);
+	sincos(fRotation.z, fRot.y, fRot.x);
+	fVert1.xy = RotateVector(fVert1.xy, fRot);
+	fVert2.xy = RotateVector(fVert2.xy, fRot);
 	// Rotate around X and Y if not a billboard
 	if(!bBillboard) {
-		sincos(fRotation.y, fRot[1].y, fRot[1].x);
-		sincos(fRotation.x, fRot[2].y, fRot[2].x);
-		fVert1.xz = RotateVector(fVert1.xz, fRot[1]);
-		fVert2.xz = RotateVector(fVert2.xz, fRot[1]);
-		fVert1.yz = RotateVector(fVert1.yz, fRot[2]);
-		fVert2.yz = RotateVector(fVert2.yz, fRot[2]);
+		sincos(fRotation.y, fRot.y, fRot.x);
+		fVert1.xz = RotateVector(fVert1.xz, fRot);
+		fVert2.xz = RotateVector(fVert2.xz, fRot);
+		sincos(fRotation.x, fRot.y, fRot.x);
+		fVert1.yz = RotateVector(fVert1.yz, fRot);
+		fVert2.yz = RotateVector(fVert2.yz, fRot);
 	}
 	
-	GOut output;
 	output.i = uiSAI;
-	output.n = normalize(cross(fVert2, fVert1));
+	sincos(fRotation.x + 1.5f, fRot.y, fRot.x);
+	output.rotX = f32tof16(fRot.y) | (f32tof16(-fRot.x) << 16);
+	sincos(fRotation.y + 1.5f, fRot.y, fRot.x);
+	output.rotY = f32tof16(fRot.y) | (f32tof16(-fRot.x) << 16);
+	sincos(fRotation.z + 1.5f, fRot.y, fRot.x);
+	output.rotZ = f32tof16(fRot.y) | (f32tof16(-fRot.x) << 16);
 
 	// Calculate animation frame offset
 	const float fFrameOS = (fTC.z - fTC.x) * trunc(((totalSecs / fFT) + fAFO) % fAFC);
-//	const float fFrameOS = 0.0f;
 
 	// Bottom-left vertex
 	output.position = fPosition; if(!bBillboard) output.position -= fVert1;
